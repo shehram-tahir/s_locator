7387c754d804444cb3dbec7dfb14b65e794f18b1
abdullah abbas <46547131+abdullahalhoothy@users.noreply.github.com>
2024-05-18 23:30:37 -0400

    uploaded files

---
+++ FrontEnd/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    //DEV
+    host: '0.0.0.0',  // DEV Ensure the server is accessible from outside the container
+    hot: true,
+    watchContentBase: true,
+    historyApiFallback: true
+  }
+};
