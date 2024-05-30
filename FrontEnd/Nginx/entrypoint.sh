73ffb5b85605ef63d0622d4ec6793037f3a2dbd2
abdulah <abdullahalhoothy92@gmail.com>
2024-05-29 23:42:32 -0400

    configs for VPS

--- FrontEnd/Nginx/entrypoint.sh
+++ FrontEnd/Nginx/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# # First, try to renew the certificate in case it's near expiration
-# certbot renew --nginx --non-interactive
+# First, try to renew the certificate in case it's near expiration
+certbot renew --nginx --non-interactive
 
-# # If no certificates exist yet, register and obtain a new one
-# if [ ! -e "/etc/letsencrypt/live/s-locator.northernacs.com/fullchain.pem" ]; then
-#     certbot --nginx -d s-locator.northernacs.com --non-interactive --agree-tos -m abdulahabbas@northernacs.com --redirect
-# fi
+# If no certificates exist yet, register and obtain a new one
+if [ ! -e "/etc/letsencrypt/live/s-locator.northernacs.com/fullchain.pem" ]; then
+    certbot --nginx -d s-locator.northernacs.com --non-interactive --agree-tos -m abdulahabbas@northernacs.com --redirect
+fi
 
 # Start Nginx in foreground
 nginx -g 'daemon off;'
