4e421150216195a6dbf6cac4d345e5906fd7102b
shehram-tahir <143114604+shehram-tahir@users.noreply.github.com>
2024-11-04 00:34:49 +0500

    FEAT: Updated schema (#15)
    * FEAT: Updated submodules
    
    * FIX: Updated schema of customers
    
    * FIX: Fixes in DB Tables
    
    * FIX: Updated schema of customers
    
    # Conflicts:
    #	storage/postgres/init.sql
    
    * FIX
    
    * FEAT: Updated schema
    
    * FEAT: Updated schema
    
    * FEAT: Updated schema
    
    * FIX Conflicts
    
    * FEAT: Updated schema
    
    * FIX Conflicts

--- storage/postgres/init.sql
+++ storage/postgres/init.sql
@@ -17,25 +17,9 @@ CREATE TABLE IF NOT EXISTS scraped_data (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS SubscriptionModels (
+CREATE TABLE IF NOT EXISTS Product (
     id SERIAL PRIMARY KEY,
-    product_id VARCHAR(255) NOT NULL UNIQUE,
-    default_price VARCHAR(255),
-    name VARCHAR(255) NOT NULL,
-    active BOOLEAN,
-    attributes TEXT[], -- PostgreSQL array type
-    caption TEXT,
-    deactivate_on TEXT[], -- PostgreSQL array type
-    description TEXT,
-    images TEXT[], -- PostgreSQL array type
-    livemode BOOLEAN,
-    metadata JSONB NOT NULL DEFAULT '{"seats": 1, "free_trial": true, "free_trial_days": 7, "can_extend_seats": false}', -- JSONB type for metadata
-    package_dimensions JSONB, -- Assuming package_dimensions can be a JSON object
-    shippable BOOLEAN,
-    statement_descriptor VARCHAR(255),
-    tax_code VARCHAR(255),
-    unit_label VARCHAR(255),
-    url TEXT
+    product_id VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS teams (
@@ -53,18 +37,7 @@ CREATE TABLE IF NOT EXISTS team_members (
 CREATE TABLE IF NOT EXISTS prices (
     id SERIAL PRIMARY KEY, -- Unique identifier for the price
     price_id VARCHAR(255) NOT NULL UNIQUE, -- ID of the price
-    product_id VARCHAR(255) NOT NULL REFERENCES SubscriptionModels(product_id) ON DELETE CASCADE, -- ID of the product
-    currency VARCHAR(3) NOT NULL, -- Currency code (e.g., USD, EUR)
-    tiers JSONB,
-    unit_amount INTEGER NOT NULL, -- Amount in the smallest currency unit (e.g., cents)
-    recurring_interval VARCHAR(50) NOT NULL, -- Interval for recurring payments (e.g., month, year)
-    recurring_interval_count INTEGER NOT NULL, -- Number of intervals between each recurring payment
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the price was created
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the price was last updated,
-    pricing_type VARCHAR(10) CHECK (pricing_type IN ('flat', 'tier')) NOT NULL,
-    base_amount INT,
-    included_seats INT,
-    additional_seat_price INT
+    product_id VARCHAR(255) NOT NULL REFERENCES Product(product_id) ON DELETE CASCADE -- ID of the product
 );
 
 
@@ -72,8 +45,6 @@ CREATE TABLE IF NOT EXISTS stripe_subscriptions (
     subscription_id VARCHAR(255) PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL,
     product_id VARCHAR(255) NOT NULL,
-    seats INT NOT NULL,
-    status VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -86,21 +57,12 @@ CREATE TABLE IF NOT EXISTS logs (
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp when the log was created
 );
 
--- wallet table for storing user wallet information
-CREATE TABLE IF NOT EXISTS wallet (
-    wallet_id SERIAL PRIMARY KEY, -- Unique identifier for the wallet
-    user_id VARCHAR(255) NOT NULL, -- ID of the user associated with the wallet
-    balance INTEGER NOT NULL, -- Current balance in the wallet
-    currency VARCHAR(3) NOT NULL DEFAULT 'USD', -- Currency code (e.g., USD, EUR)
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the wallet was created
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp when the wallet was last updated
-);
 
 -- Stripe customers
 CREATE TABLE IF NOT EXISTS stripe_customers (
     id SERIAL PRIMARY KEY, -- Primary key of table
-    customer_id VARCHAR(255) NOT NULL, -- Stripe customer ID
-    user_id VARCHAR(255) UNIQUE NOT NULL -- Reference to the user ID in your system
+    customer_id VARCHAR(255) NOT NULL UNIQUE, -- Stripe customer ID
+    user_id VARCHAR(255) UNIQUE NOT NULL UNIQUE  -- Reference to the user ID in your system
 );
 
 
@@ -115,38 +77,3 @@ CREATE TABLE IF NOT EXISTS transactions (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the transaction was created
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp when the transaction was last updated
 );
-
-
-CREATE TABLE IF NOT EXISTS stripe_payment_methods (
-    payment_method_id VARCHAR(255) PRIMARY KEY,
-    user_id VARCHAR(255) NOT NULL,
-    customer_id VARCHAR(255) NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    billing_details JSONB NOT NULL
-);
-
-
-CREATE OR REPLACE FUNCTION get_user_wallet(p_user_id VARCHAR(255))
-RETURNS TABLE (
-    wallet_id INTEGER,
-    user_id VARCHAR(255),
-    balance INTEGER,
-    currency VARCHAR(3),
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
-) AS $$
-BEGIN
-    RETURN QUERY
-    SELECT w.wallet_id, w.user_id, w.balance, w.currency, w.created_at, w.updated_at
-    FROM wallet w
-    WHERE w.user_id = (
-        SELECT COALESCE(
-            (SELECT t.owner_id
-             FROM teams t
-             JOIN team_members tm ON t.team_id = tm.team_id
-             WHERE tm.user_id = p_user_id),
-            p_user_id
-        )
-    );
-END;
-$$ LANGUAGE plpgsql;
